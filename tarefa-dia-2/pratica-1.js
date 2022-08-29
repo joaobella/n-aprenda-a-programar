839b6fd0ab47db28046276c457a5a85edad1073d
Joaobella <joaogilbertobella@gmail.com>
2022-08-29 17:17:55 -0300

    comitando mensagem de agencia de viagens com template strings

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -4,4 +4,11 @@ let profissao = "DEV"
 let idade = 38
 let viagemDosSonhos  =  "Ilhas Mauricio"
 
-console.log(nome + email + profissao + idade + viagemDosSonhos)
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(viagemDosSonhos)
+
+let mensagem = "Olá, " + nome + "!Somos aqui da agência de turismo ViajeAgora. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para " + viagemDosSonhos + ". Confirma pra gente alguns dados? a sua idade é " + idade + " , voce é " + profissao + " e o seu email é " + email + "? Ficamos no aguardo. Muito obrigado."
+console.log (mensagem)
