252f96f9dcdf2a4ebdbf1303ee60bfbd86d3d7a5
Joaobella <joaogilbertobella@gmail.com>
2022-08-29 18:50:01 -0300

    comitando tarefa3 dia2

---
+++ tarefa-dia-2/pratica-4.js
@@ -0,0 +1,15 @@
+let produtoA = {
+    "nome": "camiseta", "valor": 100.00, "internacional": true
+};
+let produtoB = {
+    "nome": "perfume", "valor": 200.00, "internacional": true
+};
+let produtoC = {
+    "nome": "sandália", "valor": 120.00, "internacional": false
+};
+
+produtoA.valor = 
+
+console.log(produtoA.valor)
+console.log(produtoB.valor)
+console.log(produtoC.valor)
